ca2d7dbedb917f26a37ca893c330a112cd3a6fa6
Joeri Leemans <joeri.leemans@monster.com>
2022-07-27 16:14:54 +0200

    second commit

--- src/main/java/eu/deltasource/githubactions/GithubActionsApplication.java
+++ src/main/java/eu/deltasource/githubactions/GithubActionsApplication.java
@@ -2,9 +2,16 @@ package eu.deltasource.githubactions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController("/welcome")
 public class GithubActionsApplication {
+  @GetMapping
+  public String welcome() {
+    return "Welcome!";
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(GithubActionsApplication.class, args);
