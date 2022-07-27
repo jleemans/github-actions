e29a3dae08594a4450033272203f106352cb7508
Joeri Leemans <joeri.leemans@monster.com>
2022-07-27 15:38:10 +0200

    first commit

---
+++ src/main/java/eu/deltasource/githubactions/GithubActionsApplication.java
@@ -0,0 +1,12 @@
+package eu.deltasource.githubactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GithubActionsApplication.class, args);
+  }
+}
